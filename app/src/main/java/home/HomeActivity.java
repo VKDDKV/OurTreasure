c22cf976d17653b4c01057594c2e71893ff34353
nguyentviet3513 <nguyentviet3513@gmail.com>
2017-12-17 22:57:11 +0900

    Initial commit

---
+++ app/src/main/java/home/HomeActivity.java
@@ -0,0 +1,15 @@
+package home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mannermakethman.vkd.ourtreasure.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
